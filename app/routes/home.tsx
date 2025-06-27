16d1ad159527a82e14981926ce0425077dde3b68
justin-0 <itsjustinoconnell@gmail.com>
2025-06-27 14:25:00 +0100

    initial

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,68 @@
+import { useState } from "react";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Form } from "react-router";
+import { signUp } from "~/lib/auth-client";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Swipe" },
+    {
+      name: "description",
+      content: "Welcome to Swipe your financial dashboard!",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signUpWithEmailAndPassword = async () => {
+    await signUp.email(
+      {
+        email,
+        password,
+        name,
+      },
+      {
+        onRequest: (ctx) => {
+          // show loading state
+        },
+        onSuccess: (ctx) => {
+          // redirect to home
+        },
+        onError: (ctx) => {
+          alert(ctx.error.message);
+        },
+      }
+    );
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <Form onSubmit={signUpWithEmailAndPassword}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Name"
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Sign Up</button>
+      </Form>
+    </div>
+  );
 }
